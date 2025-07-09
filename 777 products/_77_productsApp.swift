4955f1be5278cd431dbae8c07bb43bc8db28c946
Radcabna <radcabna@gmail.com>
2025-07-09 15:10:19 +0300

    Initial Commit

---
+++ 777 products/_77_productsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _77_productsApp.swift
+//  777 products
+//
+//  Created by Алкександр Степанов on 09.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct _77_productsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
